db0250981aa42dd8d564f86e68fe596141ddaf9a
Bui-Anh-Vy <95459666+Bui-Anh-Vy@users.noreply.github.com>
2024-01-14 18:28:43 +0700

    Create index.php

---
+++ about/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <script src="https://kit.fontawesome.com/a3c4741b90.js" crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+   <title>About us</title>
+</head>
+<body>
+   <header>
+      <div class="title">
+         <img src="../Styles/NES_logo.svg" alt="btn">
+         <a href="#">Home</a>
+      </div>
+   </header>
+
+   <div class="head">
+      <h1>About Us</h1>
+   </div>
+   
+   <div class="story">
+      <h2>Our Story</h2>
+      <p>We want to create a game review and recommend site. Unlike game review sites available on platforms, instead of users having to spend time reviewing games or details of each game to choose the right game for themselves, our website will help users save time in these operations when users only need to choose options according to suggestions,  The site will display games that match their interests. </p>
+      <p>After unsuccessfully coming up with the first portable game system idea, we considered and agreed to create a website based on the previous idea. Creating a gaming review site is currently full of websites, but what makes our site different? Save time, can find favorite games, review or objective reviews (based on google reviews), help users balance game playing and working time. </p>
+   </div>
+
+   <div class="info1">
+      <h2>Group members</h2>
+      <div class="groupInfo">
+         <div class="info">
+            <img src="./download.png" alt="">
+            <div class="infoDetail">
+               <h3>Name</h3>
+               <p>Designer</p>
+               <p class="description">Description</p>
+            </div>
+         </div>
+         <div class="info">
+            <img src="./download.png" alt="">
+            <div class="infoDetail">
+               <h3>Name</h3>
+               <p>Designer</p>
+               <p class="description">Description</p>
+            </div>
+         </div>
+         <div class="info">
+            <img src="./download.png" alt="">
+            <div class="infoDetail">
+               <h3>Name</h3>
+               <p>Designer</p>
+               <p class="description">Description</p>
+            </div>
+         </div>
+         <div class="info">
+            <img src="./download.png" alt="">
+            <div class="infoDetail">
+               <h3>Name</h3>
+               <p>Designer</p>
+               <p class="description">Description</p>
+            </div>
+         </div>
+      </div>
+   </div>
+   <footer class="section-p1">
+      <div class="col">
+          <img class="logo" src="./logo.png" alt="">
+          <h4>Contact</h4>
+          <p><strong>Address:</strong> 702 Nguyen Van Linh District 7, Ho Chi Minh City</p>
+          <p><strong>Tel:</strong> (+84) 28 3776 1369</p>
+          <p><strong>Hours:</strong> Monday to Saturday from 8am to 5pm (GMT+7)</p>
+      </div>
+      <div class="follow">
+          <h4>Follow and support us</h4>
+          <div class="icon">
+              <i class="fa-brands fa-facebook"></i>
+              <i class="fa-brands fa-instagram"></i>
+              <i class="fa-brands fa-youtube"></i>
+              <i class="fa-brands fa-twitter"></i>
+          </div>
+      </div>
+      <div class="col">
+          <h4>About</h4>
+          <a href="#">About us</a>
+          <a href="#">Terms & Conditions</a>
+          <a href="#">Contact Us</a>
+      </div>
+      <div class="col">
+          <h4>My Account</h4>
+          <a href="#">Sign In</a>
+          <a href="#">Help</a>
+      </div>
+  </footer>
+</body>
+</html>
